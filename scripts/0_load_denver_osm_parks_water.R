5f4a136a2cc17e76f936824fca3fa583e9c59517
michaeldgarber <mdg711@gmail.com>
2022-03-06 14:13:55 -0700

    prueba new git proj

--- scripts/0_load_denver_osm_parks_water.R
+++ scripts/0_load_denver_osm_parks_water.R
@@ -15,49 +15,53 @@ library(osmdata)
 #https://wiki.openstreetmap.org/wiki/Tag:leisure%3Dpark
 #https://wiki.openstreetmap.org/wiki/Category:Key_descriptions
 
-#load bounding box for den county and jeff co that was created in another script in this project
-setwd(here("data-processed"))
-load("den_jeff_co_bbox.RData")
-den_jeff_co_bbox %>% mapview()
+#load bounding box for Metro Denver that was created here:
+#~/Dropbox/CSU/green-space-denver/scripts/1a_get_landsat_ndvi_denver.R
 
-#another idea for a bounding box, similar to what I did in atlanta (i-285)
-#is to use the
+setwd(here("data-processed"))
+load("den_metro_bbox_custom.RData") #doesn't include all of southern JeffCo
+load("den_jeff_co_bbox.RData") #all of Jeff Co
+mapview(den_metro_bbox_custom) + mapview(den_jeff_co_bbox)
+#update 3/6/22 for speed use den_metro_bbox_custom instead
+
+
 # Parks--------------
 ## Download park data-----------
-#Commenting out because it takes a while
-# den_area_park = opq(bbox = den_jeff_co_bbox) %>%
+#Don't run unless you have to, as it takes about 1 min, and the IP address may have a quota
+# den_metro_park = opq(bbox = den_metro_bbox_custom) %>%
 #   add_osm_feature(key = "leisure" , value="park") %>%
 #   osmdata_sf()
 # 
-# den_area_nature_reserve = opq(bbox = den_jeff_co_bbox) %>%
+# den_metro_nature_reserve = opq(bbox = den_metro_bbox_custom) %>%
 #   add_osm_feature(key = "leisure" , value="nature_reserve") %>%
 #   osmdata_sf()
 # 
-# den_area_protected_area = opq(bbox = den_jeff_co_bbox) %>%
+# den_metro_protected_area = opq(bbox = den_metro_bbox_custom) %>%
 #   add_osm_feature(key = "boundary" , value="protected_area") %>%
 #   osmdata_sf()
 
 
-## save park data because it takes a bit---------
-# save(den_area_park, file = "den_area_park.RData")
-# save(den_area_nature_reserve, file = "den_area_nature_reserve.RData")
-# save(den_area_protected_area, file = "den_area_protected_area.RData")
+## save the park data from OSM ---------
+# save(den_metro_park, file = "den_metro_park.RData")
+# save(den_metro_nature_reserve, file = "den_metro_nature_reserve.RData")
+# save(den_metro_protected_area, file = "den_metro_protected_area.RData")
 
 #load rather than re-downloading unless the bounding box changes
-load("den_area_park.RData")
-load("den_area_nature_reserve.RData")
-load("den_area_protected_area.RData")
+load("den_metro_park.RData")
+load("den_metro_nature_reserve.RData")
+load("den_metro_protected_area.RData")
 
 
-#----------wrangle downloaded park data--------------------#
+## wrangle park data before combining-----------
+
 #select the polygons nad multipolygons from the OSM object
-den_area_park_polygons = den_area_park$osm_polygons %>% 
+den_metro_park_polygons = den_metro_park$osm_polygons %>% 
   mutate(osm_origin_feature_type = "polygon")
-den_area_park_multipolygons = den_area_park$osm_multipolygons  %>% 
+den_metro_park_multipolygons = den_metro_park$osm_multipolygons  %>% 
   mutate(osm_origin_feature_type = "multipolygon")
 
-den_area_park_resolved = den_area_park_polygons %>% 
-  bind_rows(den_area_park_multipolygons) %>% 
+den_metro_park_resolved = den_metro_park_polygons %>% 
+  bind_rows(den_metro_park_multipolygons) %>% 
   st_as_sf() %>% 
   #add a couple variables to keep track of source
   mutate(
@@ -65,32 +69,32 @@ den_area_park_resolved = den_area_park_polygons %>%
     osm_value = "park"
   )
 
-#den_area_park_resolved %>% mapview()
+#den_metro_park_resolved %>% mapview()
 
 #do the same for the nature reserve
-den_area_nature_reserve_polygons = den_area_nature_reserve$osm_polygons %>% 
+den_metro_nature_reserve_polygons = den_metro_nature_reserve$osm_polygons %>% 
   mutate(osm_origin_feature_type = "polygon")
-den_area_nature_reserve_multipolygons = den_area_nature_reserve$osm_multipolygons %>% 
+den_metro_nature_reserve_multipolygons = den_metro_nature_reserve$osm_multipolygons %>% 
   mutate(osm_origin_feature_type = "multipolygon")
 
 
-den_area_nature_reserve_resolved = den_area_nature_reserve_polygons %>% 
-  bind_rows(den_area_nature_reserve_multipolygons) %>% 
+den_metro_nature_reserve_resolved = den_metro_nature_reserve_polygons %>% 
+  bind_rows(den_metro_nature_reserve_multipolygons) %>% 
   st_as_sf() %>% 
   #add a couple variables to keep track of source
   mutate(
     osm_key = "leisure",
     osm_value = "nature_reserve"
   )
-#den_area_nature_reserve_resolved %>% mapview()
+#den_metro_nature_reserve_resolved %>% mapview()
 
-den_area_protected_area_polygons = den_area_protected_area$osm_polygons %>% 
+den_metro_protected_area_polygons = den_metro_protected_area$osm_polygons %>% 
   mutate(osm_origin_feature_type = "polygon")
-den_area_protected_area_multipolygons = den_area_protected_area$osm_multipolygons %>% 
+den_metro_protected_area_multipolygons = den_metro_protected_area$osm_multipolygons %>% 
   mutate(osm_origin_feature_type = "multipolygon")
 
-den_area_protected_area_resolved = den_area_protected_area_polygons %>% 
-  bind_rows(den_area_protected_area_multipolygons) %>% 
+den_metro_protected_area_resolved = den_metro_protected_area_polygons %>% 
+  bind_rows(den_metro_protected_area_multipolygons) %>% 
   st_as_sf() %>% 
   #add a couple variables to keep track of source
   mutate(
@@ -104,10 +108,10 @@ den_area_protected_area_resolved = den_area_protected_area_polygons %>%
 load("den_jeff_co_geo.RData")
 #note name change to specify that it's just these two counties
 #update to change to green space (omit the word public for brevity)
-den_jeff_co_green_space =den_area_park_resolved %>% 
+den_jeff_co_green_space =den_metro_park_resolved %>% 
   bind_rows(
-    den_area_nature_reserve_resolved,
-    den_area_protected_area_resolved
+    den_metro_nature_reserve_resolved,
+    den_metro_protected_area_resolved
   ) %>% 
   st_make_valid() %>% #needed because there are some duplicate vertices
   #restrict to just the bounding box, as it was picking up many features
@@ -134,22 +138,112 @@ load("den_jeff_co_green_space.RData")
 den_jeff_co_green_space %>% mapview(zcol = "osm_value")
 # Bodies of water------------
 ## Download bodies of water---------------
-# den_area_water = opq(bbox = den_jeff_co_bbox) %>%
-#   add_osm_feature(key = "natural" , value="water") %>%
-#   osmdata_sf()
-# save(den_area_water, file = "den_area_water.RData")
-
-load("den_area_water.RData")
-den_area_water_polygons = den_area_water$osm_polygons %>% 
-  mutate(osm_origin_feature_type = "polygon")
-den_area_water_multipolygons = den_area_water$osm_multipolygons %>% 
-  mutate(osm_origin_feature_type = "multipolygon")
-den_area_water_lines = den_area_water$osm_lines %>% 
-  mutate(osm_origin_feature_type = "lines") #no area by definition so doesn't matter
+#A few different ways:
+### key natural value water ----------
+#https://wiki.openstreetmap.org/wiki/Tag:natural%3Dwater
+den_metro_natural_water= opq(bbox = den_metro_bbox_custom) %>%
+  add_osm_feature(key = "natural" , value="water") %>%
+  osmdata_sf()
+setwd(here("data-processed"))
+save(den_metro_natural_water, file = "den_metro_natural_water.RData")
 
 
-den_area_water_resolved = den_area_water_polygons %>% 
-  bind_rows(den_area_water_multipolygons) %>% 
+### key waterway value river & stream ----------
+#Another way to get waterways:
+#https://wiki.openstreetmap.org/wiki/Key:waterway
+den_metro_waterway_river= opq(bbox = den_metro_bbox_custom) %>%
+  add_osm_feature(key = "waterway" , value="river") %>%
+  osmdata_sf()
+setwd(here("data-processed"))
+save(den_metro_waterway_river, file = "den_metro_waterway_river.RData")
+
+den_metro_waterway_stream= opq(bbox = den_metro_bbox_custom) %>%
+  add_osm_feature(key = "waterway" , value="stream") %>%
+  osmdata_sf()
+save(den_metro_waterway_stream, file = "den_metro_waterway_stream.RData")
+
+#Test the different sf feature types
+den_metro_waterway_river$osm_lines %>% mapview() #yes, many
+den_metro_waterway_river$osm_multilines %>% mapview() #yes, many
+den_metro_waterway_river$osm_polygons %>% mapview() #none
+den_metro_waterway_river$osm_multipolygons #null
+#den_metro_waterway_river$osm_points %>% mapview()#there but don't use
+
+### Mapview all of them together--------
+#### mapview  natural water-----------
+#I'm curious what's happening with the line-based features. I'd like to avoid using
+#them if possible.
+mv_natural_water_lines = den_metro_natural_water_lines %>% 
+  mapview(layer.name = "lines, natural, water", color = "cadetblue1")
+mv_natural_water_polygons = den_metro_natural_water_polygons %>% 
+  mapview(layer.name = "polygons, natural, water", color = "blue", col.regions = "blue")
+mv_natural_water_multipolygons = den_metro_natural_water_multipolygons %>% 
+  mapview(layer.name = "multipolygons, natural, water", color = "green", col.regions = "green")
+
+mv_natural_water_polygons+mv_natural_water_multipolygons+mv_natural_water_lines
+#Great, all of the line-based features are also polygon-based features, so we can omit
+#these lines. and are the polygons and multipolygons the same?
+#they're almost the same, but there are a few features that are not represented
+#by polygons but are represented by multipolygons. 
+#otherwise, polygons is much more complete.
+
+#### mapview waterway river------------------
+#Do they overlap perfectly? or does one contain information the other does not?
+mv_waterway_river_lines = den_metro_waterway_river$osm_lines %>% 
+  mapview(layer.name = "lines, waterway, river", color = "red")
+mv_waterway_river_multilines = den_metro_waterway_river$osm_multilines %>% 
+  mapview(layer.name = "multilines, waterway, river", color = "green")
+
+mv_waterway_river_multilines+mv_waterway_river_lines
+#osm_lines is more complete. forget multilines
+
+#### mapview waterway stream------
+mv_waterway_stream_lines = den_metro_waterway_stream$osm_lines %>% 
+  mapview(layer.name = "lines, waterway, stream", color = "purple")
+mv_waterway_stream_multilines = den_metro_waterway_stream$osm_multilines %>% 
+  mapview(layer.name = "multilines, waterway, stream", color = "green") #null
+
+mv_waterway_stream_lines+mv_waterway_stream_multilines
+
+#natural water
+mv_natural_water_polygons+
+  mv_natural_water_multipolygons+
+  mv_natural_water_lines +
+  #waterway river
+  mv_waterway_river_lines +
+  mv_waterway_stream_lines
+
+#Summary of mapviews here:
+#There are several streams that are not otherwise noted by polygons.
+#Streams indicate a continuous flow, which sometimes include bodies of water as well.
+#The data structure includes overlapping features, which makes sense. It's true
+#that a reservoir may be part of a longer river, so both the river and the reservoir
+#should be represented.
+
+#For those rivers and streams that are only represented by lines, how should
+#I indicate their area? Maybe say 10 feet?
+
+
+
+### Create sf objects for each---------
+#### sf objects for natural water-----------
+den_metro_natural_water_polygons = den_metro_natural_water$osm_polygons %>% 
+  mutate(
+    osm_key = "natural",
+    osm_value = "water",
+    osm_origin_feature_type = "polygon")
+den_metro_natural_water_multipolygons = den_metro_natural_water$osm_multipolygons %>% 
+  mutate(
+    osm_key = "natural",
+    osm_value = "water",
+    osm_origin_feature_type = "multipolygon"
+  )
+#don't create an sf object for den_metro_natural_water$osm_lines, per above checks
+
+#
+
+den_metro_natural_water_resolved = den_metro_water_polygons %>% 
+  bind_rows(den_metro_water_multipolygons) %>% 
   st_as_sf() %>% 
   #add a couple variables to keep track of source
   mutate(
@@ -157,37 +251,38 @@ den_area_water_resolved = den_area_water_polygons %>%
     osm_value = "water"
   )
 
-#den_area_water_resolved %>% mapview(zcol = "osm_origin_feature_type")
-save(den_area_water_resolved, file = "den_area_water_resolved.RData")
+#den_metro_water_resolved %>% mapview(zcol = "osm_origin_feature_type")
+save(den_metro_water_resolved, file = "den_metro_water_resolved.RData")
 
 ## create a union of all of this for an easy int-----------
 #warning: this takes forever (about 10 minutes)
 # union_method_1_t_start = Sys.time()
-# den_area_water_resolved_union_method_1 = den_area_water_resolved %>%
+# den_metro_water_resolved_union_method_1 = den_metro_water_resolved %>%
 #   st_union() 
 # union_method_1_t_end = Sys.time()
 # union_method_1_t_elapsed = union_method_1_t_end-union_method_1_t_start #7 minutes
-# save(den_area_water_resolved_union_method_1 , file = "den_area_water_resolved_union_method_1.RData")
+# save(den_metro_water_resolved_union_method_1 , file = "den_metro_water_resolved_union_method_1.RData")
 # 
-# class(den_area_water_resolved_union_method_1)
+# class(den_metro_water_resolved_union_method_1)
 # union_method_2_t_start = Sys.time()
-# den_area_water_resolved_union_method_2 = den_area_water_resolved %>%
+# den_metro_water_resolved_union_method_2 = den_metro_water_resolved %>%
 #   mutate(dummy=1) %>% 
 #   group_by(dummy) %>% 
 #   summarise(n=n()) %>% 
 #   ungroup() 
 # union_method_2_t_end = Sys.time()
 # union_method_2_t_elapsed = union_method_2_t_end-union_method_2_t_start #7 min
-# save(den_area_water_resolved_union_method_2, file = "den_area_water_resolved_union_method_2.RData")
+# save(den_metro_water_resolved_union_method_2, file = "den_metro_water_resolved_union_method_2.RData")
 # class(den_area)
-load("den_area_water_resolved_union_method_1.RData")
-load("den_area_water_resolved_union_method_2.RData")
-den_area_water_resolved_union_method_2 %>% mapview()
+load("den_metro_water_resolved_union_method_1.RData")
+load("den_metro_water_resolved_union_method_2.RData")
+den_metro_water_resolved_union_method_2 %>% mapview()
+
+
 # Remove bodies of water from parks-----------
 #use st_difference https://cran.r-project.org/web/packages/sf/vignettes/sf3.html
-
 den_jeff_co_green_space_no_water = den_jeff_co_green_space %>% 
-  st_difference(den_area_water_resolved_union_method_2) %>% 
+  st_difference(den_metro_water_resolved_union_method_2) %>% 
   st_make_valid() #this did the trick!
 
 save(den_jeff_co_green_space_no_water, file = "den_jeff_co_green_space_no_water.RData")
---
+++ scripts/0_read_denver_water.R
@@ -0,0 +1,112 @@
+#filename: 0_read_denver_water
+library(here)
+library(tidyverse)
+library(sf)
+library(mapview)
+# Note in this script (~/scripts/0_load_denver_osm_parks_water.R), 
+# we load OpenStreetMap data with bodies of water.
+setwd(here("data-processed"))
+load("den_area_water_resolved.RData")
+
+#useful to have this so I know where county boundaries are
+#~/Dropbox/CSU/green-space-denver/scripts/0_import_manage_denver_acs.R
+load("den_metro_co_geo.RData")
+mapview(den_area_water_resolved)+mapview(den_metro_co_geo)
+
+#This script will load that data as well as load the water data from the
+#Denver Open Data page:
+#https://www.denvergov.org/opendata/dataset/city-and-county-of-denver-lakes-and-ponds
+#https://www.denvergov.org/opendata/dataset/city-and-county-of-denver-streams
+
+# Load data from Denver Open Data Portal----------
+## Load lakes and ponds------------
+
+#dsn = #the folder where the shapefile is located
+setwd(here("data-input", "city-of-denver-data", "bodies-of-water"))
+den_lakes_ponds = st_read(dsn ="lakes_and_ponds") %>%
+  st_transform(4326) %>%
+  st_make_valid() %>% 
+  rename(
+    object_id = OBJECTID_1,
+    object_name = LAKE_NAME #make it more general since we already know it's a lake
+    ) %>% 
+  #these are all L&P, i.e., Lakes and Ponds.
+  #in the river version, they call it UNITTYPE.
+  #let's resolve that all under the same variable.
+  mutate(
+    unit_type = "lake or pond",
+    unit_type_broad = "lake or pond" #for consistency with below
+    ) %>% 
+  dplyr::select(starts_with("object"), contains("type"))
+
+setwd(here("data-processed"))
+save(den_lakes_ponds, file = "den_lakes_ponds.RData")
+den_lakes_ponds %>% mapview()
+## Load streams--------
+setwd(here("data-input", "city-of-denver-data", "bodies-of-water"))
+den_streams = st_read(dsn ="streams") %>%
+  st_transform(4326) %>%
+  st_make_valid() %>% 
+  rename(
+    object_id = OBJECTID_1,
+    object_name = STREAM_NAM, #make it more general since we already know it's a stream
+    unit_type = UNITTYPE,
+    stream_type = STREAM_TYP
+    ) %>% 
+  mutate(unit_type_broad = "streams") %>% 
+  dplyr::select(starts_with("object"), contains("type"))  
+
+den_streams %>% mapview()
+class(den_streams$geometry)
+class(den_lakes_ponds$geometry)
+
+#note this won't work with mapview because it's of type mixed geometry, but still worth
+#creating this so you can use st_buffer on object instead of multiple
+den_streams_lakes_ponds = den_streams %>% 
+  bind_rows(den_lakes_ponds) 
+
+table(den_streams_lakes_ponds$unit_type)
+den_streams_lakes_ponds %>% 
+  filter(unit_type_broad == "streams") %>% 
+  mapview(layer.name = "test")
+setwd(here("data-processed"))
+save(den_streams_lakes_ponds, file = "den_streams_lakes_ponds.RData")
+
+
+# Define buffers per policy suggestion---------
+#200 feet ideal, 100 good, 50 feet realistic
+#convert to meters since we're in 4326
+#1 meter = 3.28084 feet
+ft_in_m_200 = 200/3.28084 #60.96 meters
+ft_in_m_100 = 100/3.28084
+ft_in_m_50 = 50/3.28084
+
+den_streams_lakes_ponds_200_ft = den_streams_lakes_ponds %>% 
+  st_buffer(ft_in_m_200)
+den_streams_lakes_ponds_100_ft = den_streams_lakes_ponds %>% 
+  st_buffer(ft_in_m_100)
+den_streams_lakes_ponds_50_ft = den_streams_lakes_ponds %>% 
+  st_buffer(ft_in_m_50)
+
+# Removies bodies of water from buffers-------
+#Note that the streams are lines, so they won't have an area. We need to remove
+#the bodies of water loaded from OpenStreetMap
+class(den_streams$geometry)
+#To pare it down, since we're just in Denver, limit the OSM water data to
+#Denver proper. This is created here: 
+#~/Dropbox/CSU/green-space-denver/scripts/0_import_manage_denver_acs.R
+setwd(here("data-processed"))
+load("den_area_water_resolved.RData") #created in 0_import_manage_denver_acs.R
+load("den_co_geo.RData")
+
+den_streams_lakes_ponds_50_ft %>% mapview()
+#the OSM data just in Denver County
+den_co_water_osm_resolved = den_area_water_resolved %>% 
+  st_intersection(den_co_geo)
+
+#The difference between the buffers and the bodies of water
+den_water_200_ft_diff = den_streams_lakes_ponds_200_ft %>% 
+  st_difference(den_co_water_osm_resolved)
+
+
+
