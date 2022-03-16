e58f356921e647422a1eeb8c7f6d8a530e3a0c1b
michaeldgarber <mdg711@gmail.com>
2022-03-16 14:06:38 -0600

    replace water source comparison code

---
+++ scripts/1_compare_water_sources.R
@@ -0,0 +1,30 @@
+#filename: 1_wrangle_water
+
+#This is an extension of these two:
+
+  #~/green-space-denver/scripts/0_read_denver_water.R
+  #~/green-space-denver/scripts/0_load_denver_osm_parks_water.R
+
+#The first script creates, the following, which includes all of the polygons, 
+#non-overlapping, as well as the streams and rivers represented 
+#with 10-foot buffers around them if they are otherwise
+#represented as a line to give them some area.
+setwd(here("data-processed"))
+load("den_osm_water_poly_inc_waterways_10_ft.RData")
+
+#The second reads in the data from the Denver portal, including both streams and rivers
+#and lakes and ponds.
+load("den_streams_lakes_ponds.RData")
+
+# Compare waterways obtained from OSM with that of Denver Portal ----------
+#My purpose here is to compare them and come up with a final dataset for the creation of the riparian
+#buffers.
+
+mv_den_streams_lakes_ponds = den_streams_lakes_ponds %>% 
+  mapview(color = "firebrick1", col.regions = "firebrick1", layer.name = "Denver Portal")
+
+mv_den_osm_water_poly_inc_waterways_10_ft = den_osm_water_poly_inc_waterways_10_ft %>% 
+  mapview(color = "blue", col.regions = "blue", layer.name = "OSM")
+
+mv_den_streams_lakes_ponds + mv_den_osm_water_poly_inc_waterways_10_ft 
+
